fc29407b34becb1e58457a3a127d3e5d6a6e57cd
Jack <jbrigleb@uoregon.edu>
2018-08-16 13:31:35 -0700

    Clear Repo
    clearing all files first. Will copy in and commit with a gitignore after

--- Assets/Scripts/LevelClass.cs
+++
@@ -1,9 +0,0 @@
-﻿public class LevelClass {
-	
-	public int[] map_dimensions;
-
-	public LevelClass(int mapX,int mapY) {
-		map_dimensions = new int[] {mapX,mapY};
-	}
-	
-}
--- Assets/Scripts/TileArrayScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TileArrayScript : MonoBehaviour {
-	
-	// a two-dimensional array of GameObjects used for storing the tile grid
-	public GameObject[,] tArray;
-	
-}
